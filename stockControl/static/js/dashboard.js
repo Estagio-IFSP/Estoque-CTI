a9c882b6378042cc9891632dfcef3d30e09127f2
jultty <juno.t@tuta.io>
2024-08-07 14:56:56 -0300

    Fix consumable goods toggle not working at first

--- stockControl/static/js/dashboard.js
+++ stockControl/static/js/dashboard.js
@@ -1,14 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const consumableButton = document.querySelector('.consumable-button');
-    const tableConsumable = document.querySelector('.tableconsumable');
+  const consumableButton = document.querySelector('.consumable-button');
+  const tableConsumable = document.querySelector('.tableconsumable');
 
-    
-    consumableButton.addEventListener('click', function () {
-      // Alterna a visibilidade da tabela ao clicar no botão
-      if (tableConsumable.style.display === 'none') {
-        tableConsumable.style.display = 'block';
-      } else {
-        tableConsumable.style.display = 'none';
-      }
-    });
+  consumableButton.addEventListener('click', function () {
+    // Alterna a visibilidade da tabela ao clicar no botão
+
+    const styles = window.getComputedStyle(tableConsumable);
+
+    if (styles.getPropertyValue("display") === 'none') {
+      tableConsumable.style.display = 'block';
+    } else {
+      tableConsumable.style.display = 'none';
+    }
   });
+});

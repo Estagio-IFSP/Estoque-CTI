20842e3a6377064386996eb86fb3a9d6bd9ba455
jultty <juno.t@tuta.io>
2024-08-08 16:29:02 -0300

    Remodel according to requirements

--- stockControl/admin.py
+++ stockControl/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import ConsumableGood, PermanentGood, Stock, Supplier
+from .models import Good, ConsumableGood, PermanentGood, Stock, Supplier, Warranty, Loan
 
+admin.site.register(Good)
 admin.site.register(ConsumableGood)
 admin.site.register(PermanentGood)
 admin.site.register(Stock)
 admin.site.register(Supplier)
+admin.site.register(Warranty)
+admin.site.register(Loan)
--- stockControl/models.py
+++ stockControl/models.py
@@ -1,28 +1,49 @@
 from django.db import models
 
-# Bens permanentes e de consumo
-class ConsumableGood(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    status = models.CharField(max_length=30)
-    quantidade = models.PositiveBigIntegerField()
 
-class PermanentGood(models.Model):
+# Fornecedor
+class Supplier(models.Model):
     name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=20)
+
+# Garantia
+class Warranty(models.Model):
+    expiryDate = models.DateField()
+    details = models.TextField();
+
+# Bem (permanente e de consumo
+class Good(models.Model):
+    name = models.CharField(max_length=100)
+    acquisitionDate = models.DateField()
+    quantity = models.PositiveBigIntegerField()
     description = models.TextField()
     status = models.CharField(max_length=30)
+    supplier = models.ForeignKey(Supplier, on_delete = models.PROTECT)
+
+# Bem de consumo
+class ConsumableGood(Good):
+    pass
+
+# Bem permanente
+class PermanentGood(Good):
+    patrimony = models.BooleanField()
+    warranty = models.ManyToManyField(Warranty)
+
+# Pessoa que empresta um bem
+class Claimant(models.Model):
+    identifier = models.CharField()
+    phone_number = models.PositiveIntegerField()
+
+# Empréstimo
+class Loan(models.Model):
+    good = models.ForeignKey(Good, on_delete = models.PROTECT)
     loanDate = models.DateField()
-    claimant = models.CharField(max_length=50)
+    returnDate = models.DateField()
+    quantity = models.PositiveBigIntegerField()
+    claimant = models.ForeignKey(Claimant, on_delete = models.PROTECT)
 
 # Estoque
 class Stock(models.Model):
     quantity = models.PositiveBigIntegerField()
     category = models.CharField(max_length=50)
     actionDate = models.DateField()
-
-#Fornecedor
-class Supplier(models.Model):
-    name = models.CharField(max_length=100)
-    phone = models.CharField(max_length=20)
-
-

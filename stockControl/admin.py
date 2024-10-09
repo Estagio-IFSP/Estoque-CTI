d68983aab4af2d22dde431116ffe450959213480
jultty <juno.t@tuta.io>
2024-10-09 15:14:00 -0300

    Register LoanItem model in the Django Admin

--- stockControl/admin.py
+++ stockControl/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Good, Claimant, Supplier, Loan
+from .models import Good, Claimant, Supplier, Loan, LoanItem
 
 admin.site.register(Good)
 admin.site.register(Claimant)
 admin.site.register(Supplier)
 admin.site.register(Loan)
+admin.site.register(LoanItem)

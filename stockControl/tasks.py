0092510540b644855cd3ed7491013cf2a152440c
jultty <juno.t@tuta.io>
2024-10-30 13:01:47 -0300

    Fix mail sending logic

--- stockControl/tasks.py
+++ stockControl/tasks.py
@@ -2,28 +2,25 @@ from huey import crontab
 from huey.contrib import djhuey
 from stockControl.settings import HUEY as huey
 from stockControl.models import LoanItem
+from django.db.models import Q
 from django.core.mail import send_mail
 from django.conf import settings
+from datetime import date, timedelta
 
-@huey.periodic_task(crontab(minute='*'))
-def periodic_task():
-    print('Scheduled tasks are running')
-
-# Disparar emails para CTI e para requerente
-#   - 5 dias úteis antes do vencimento
-#   - 10 dias úteis antes do vencimento
-# Disparar emails após atraso para a CTI e para requerente
-
-def send_due_soon_email(item):
-    send_mail(
+def send_notification_email(item):
+    if send_mail(
         "[Estoque-CTI] Seu empréstimo vence em " + str(item.get_days_before_due()) + " dias",
         "Seu empréstimo de " + str(item) + " vence em " + str(item.get_days_before_due()) \
-            + " dias. Por favor, devolva-o até " + item.loan.return_date.strftime("%d/%m/%Y") \
-            + " ou solicite a extensão do prazo.",
+            + " dias. Por favor, devolva-o até " + item.loan.return_date.strftime("%d/%m/%Y") + ".",
         settings.DEFAULT_FROM_EMAIL,
         [item.loan.claimant.email],
         fail_silently=False,
-    )
+    ) == 1:
+        item.loan.last_notification_email = date.today()
+        item.loan.save()
+        print("Message successfully delivered to " + item.loan.claimant.email)
+    else:
+        print("Error delivering message to " + item.loan.claimant.email)
 
 @djhuey.db_periodic_task(
     crontab(day="*", hour="*", minute="*"),
@@ -33,19 +30,26 @@ def db_periodic_task():
 
     print('-----------------------------------------------------------------------')
 
-    loan_items = LoanItem.objects.filter(returned=False)
+    total_items = LoanItem.objects.all().count()
+    print("Total items: " + str(total_items))
 
-    for item in loan_items:
-        if item.due_check():
-            print("Due:   " + str(item) + " from " + str(item.loan) + " by " + str(abs(item.get_days_before_due())) + " days")
-        elif item.get_days_before_due() == 10:
-            print("In 10: " + str(item) + " from " + str(item.loan) + " in " + str(item.get_days_before_due()) + " days")
-            send_due_soon_email(item)
-        elif item.get_days_before_due() == 5:
-            print("In 5:  " + str(item) + " from " + str(item.loan) + " in " + str(item.get_days_before_due()) + " days")
-            send_due_soon_email(item)
-        else:
-            print("OK:    " + str(item) + " from " + str(item.loan))
+    not_due_yet_items = LoanItem.objects.filter(
+        Q(returned=False, loan__return_date__gt=date.today())
+    )
+    print("Items not due yet: " + str(not_due_yet_items.count()))
 
-    print("Unreturned items: " + str(loan_items.count()))
+    due_in_10_days = not_due_yet_items.filter(loan__last_notification_email=None, loan__return_date__lte=date.today() + timedelta(days=10))
+    print("Items due in 10 days: " + str(due_in_10_days.count()))
+
+    due_in_5_days = not_due_yet_items.filter(~Q(loan__last_notification_email=None), loan__last_notification_email__lte=date.today() - timedelta(days=5))
+    print("Items due in 5 days: " + str(due_in_5_days.count()))
+
+    for item in due_in_5_days:
+        print(" > [for due in 5] Sending email regarding " + str(item) + " from " + str(item.loan) + " due in " + str(abs(item.get_days_before_due())) + " days ")
+        send_notification_email(item)
+
+    for item in due_in_10_days:
+        print(" > [for due in 10] Sending email regarding " + str(item) + " from " + str(item.loan) + " due in " + str(abs(item.get_days_before_due())) + " days ")
+        send_notification_email(item)
+
     print('-----------------------------------------------------------------------')
